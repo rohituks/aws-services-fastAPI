f6dc88c174c1fe987acac0b1cc0d968fbf3753f1
rohituks <rohituks@gmail.com>
2023-01-24 18:55:34 +0000

    included cli fire api

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="War Goddess", length=1):
     """This is wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name):
+    """Search wikipedia for names"""
+    results = wikipedia.search(name)
+    return results
